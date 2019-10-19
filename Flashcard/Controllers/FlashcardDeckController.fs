12f4f62fab8e7ae36a08b7247e3082602d587cb5
Abraham Hosch <hosch250@live.com>
2019-10-18 20:40:51 -0500

    Add project files.

---
+++ Flashcard/Controllers/FlashcardDeckController.fs
@@ -0,0 +1,15 @@
+﻿namespace Flashcard.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open FlashcardTypes
+
+[<ApiController>]
+[<Route("[controller]")>]
+type FlashcardDeckController (logger : ILogger<FlashcardDeckController>) =
+    inherit ControllerBase()
+
+    [<HttpPost>]
+    member __.Post(flashcardDeck : FlashcardDeck) =
+        logger.LogInformation(flashcardDeck.Title) |> ignore
+        ()
---
+++ Flashcard/Flashcard.fs
@@ -0,0 +1,15 @@
+﻿module FlashcardTypes
+
+type Flashcard(id, label, front, back) =
+    member this.Id: int = id
+    member this.Label: string = label
+    member this.Front: string = front
+    member this.Back: string = back
+    new() = Flashcard(1, "", "", "")
+
+type FlashcardDeck(id, title, cards, tags) =
+    member this.Id: int = id
+    member this.Title: string = title
+    member this.Cards: Flashcard list = cards
+    member this.Tags: string list = tags
+    new() = FlashcardDeck(0, "", [], []);
