2c3c65264dd2ec146c15027a57535f4146b0f72f
Abraham Hosch <hosch250@live.com>
2019-10-26 11:35:34 -0500

    Make card columns adjust for screen size

--- Flashcard/ClientApp/src/app/deck-finder/deck-finder.component.ts
+++ Flashcard/ClientApp/src/app/deck-finder/deck-finder.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FlashcardDeckService, FlashcardDeck } from '../shared/services/flashcardDeck.service';
 import { HttpClient } from '@angular/common/http';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { findKey } from 'lodash';
 
 @Component({
     selector: 'fcd-deck-finder',
@@ -8,8 +10,32 @@ import { HttpClient } from '@angular/common/http';
     styleUrls: ['./deck-finder.component.scss']
 })
 export class DeckFinderComponent implements OnInit {
+    public columns: number;
+    private readonly breakpointsToColumns = {
+        "(max-width: 599.99px) and (orientation: portrait)": 1,
+        "(min-width: 600px) and (max-width: 839.99px) and (orientation: portrait)": 1,
+        "(min-width: 840px) and (orientation: portrait)": 1,
+        "(max-width: 959.99px) and (orientation: landscape)": 1,
+        "(min-width: 960px) and (max-width: 1279.99px) and (orientation: landscape)": 2,
+        "(min-width: 1280px) and (orientation: landscape)": 3
+    };
 
-    constructor(private readonly flashcardDeckService: FlashcardDeckService, private http: HttpClient) { }
+    constructor(private readonly flashcardDeckService: FlashcardDeckService, private http: HttpClient, private breakpointObserver: BreakpointObserver) {
+        breakpointObserver.observe([
+            Breakpoints.Handset,
+            Breakpoints.Tablet,
+            Breakpoints.Web,
+            Breakpoints.HandsetPortrait,
+            Breakpoints.TabletPortrait,
+            Breakpoints.WebPortrait,
+            Breakpoints.HandsetLandscape,
+            Breakpoints.TabletLandscape,
+            Breakpoints.WebLandscape
+        ]).subscribe(result => {
+            let breakpoint = findKey(result.breakpoints, o => o);
+            this.columns = this.breakpointsToColumns[breakpoint];
+        });
+    }
 
     private _decks: FlashcardDeck[];
     public get decks() {
