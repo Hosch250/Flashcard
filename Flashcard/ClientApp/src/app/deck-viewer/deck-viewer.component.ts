2d3e4b3895cce046aeb32f3dd477e997da7db2a0
Abraham Hosch <abraham.hosch@ilmservice.com>
2019-12-30 10:56:29 -0600

    Prevent creating a note if the user isn't logged in

--- Flashcard/ClientApp/src/app/deck-viewer/deck-viewer.component.ts
+++ Flashcard/ClientApp/src/app/deck-viewer/deck-viewer.component.ts
@@ -5,52 +5,65 @@ import { FlashcardDeck } from '../shared/models/flashcardDeck';
 import { shuffle } from 'lodash';
 
 @Component({
-    selector: 'fcd-deck-viewer',
-    templateUrl: './deck-viewer.component.html',
-    styleUrls: ['./deck-viewer.component.scss']
+  selector: 'fcd-deck-viewer',
+  templateUrl: './deck-viewer.component.html',
+  styleUrls: ['./deck-viewer.component.scss']
 })
 export class DeckViewerComponent implements OnInit {
 
-    private _flashcardDeck: FlashcardDeck;
-    public get flashcardDeck() {
-        return this._flashcardDeck;
-    }
-    public set flashcardDeck(value) {
-        this._flashcardDeck = value;
-    }
+  private _flashcardDeck: FlashcardDeck;
+  public get flashcardDeck() {
+    return this._flashcardDeck;
+  }
+  public set flashcardDeck(value) {
+    this._flashcardDeck = value;
+  }
 
-    public selectedCardIndex: number = 0;
+  public selectedCardIndex: number = 0;
 
-    constructor(private flashcardDeckService: FlashcardDeckService, private route: ActivatedRoute) {
-    }
+  constructor(private flashcardDeckService: FlashcardDeckService, private route: ActivatedRoute) {
+  }
 
-    public previousCard() {
-        if (this.selectedCardIndex === 0) {
-            return;
-        }
-
-        document.getElementsByClassName('flip-container').item(0).classList.remove('hover');
-        this.selectedCardIndex--;
+  public previousCard() {
+    if (this.selectedCardIndex === 0) {
+      return;
     }
 
-    public nextCard() {
-        if (this.selectedCardIndex === this.flashcardDeck.cards.length - 1) {
-            return;
-        }
+    document.getElementsByClassName('flip-container').item(0).classList.remove('hover');
+    this.selectedCardIndex--;
+  }
 
-        document.getElementsByClassName('flip-container').item(0).classList.remove('hover');
-        this.selectedCardIndex++;
+  public nextCard() {
+    if (this.selectedCardIndex === this.flashcardDeck.cards.length - 1) {
+      return;
     }
 
-    public shuffle() {
-        this.flashcardDeck.cards = shuffle(this.flashcardDeck.cards);
-    }
+    document.getElementsByClassName('flip-container').item(0).classList.remove('hover');
+    this.selectedCardIndex++;
+  }
 
-    ngOnInit() {
-        this.flashcardDeckService
-            .get(parseInt(this.route.snapshot.paramMap.get('id')))
-            .subscribe(data => {
-                this.flashcardDeck = data;
-            });
+  public shuffle() {
+    this.flashcardDeck.cards = shuffle(this.flashcardDeck.cards);
+  }
+
+  ngOnInit() {
+    this.flashcardDeckService
+      .get(parseInt(this.route.snapshot.paramMap.get('id')))
+      .subscribe(data => {
+        this.flashcardDeck = data;
+      });
+  }
+
+  private getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookies = decodedCookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.indexOf(name) == 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
     }
+    return "";
+  }
 }
--- Flashcard/ClientApp/src/app/note-creator/note-creator.component.ts
+++ Flashcard/ClientApp/src/app/note-creator/note-creator.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NoteService } from '../shared/services/note.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
@@ -7,34 +7,25 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: './note-creator.component.html',
   styleUrls: ['./note-creator.component.scss']
 })
-export class NoteCreatorComponent {
+export class NoteCreatorComponent implements OnInit {
 
-  constructor(fb: FormBuilder, private readonly noteService: NoteService) {
-    this.form = fb.group({
-      content: fb.control('', { validators: [Validators.required, Validators.minLength(15), Validators.maxLength(2000)], updateOn: 'blur' })
-    });
+  constructor(private readonly fb: FormBuilder, private readonly noteService: NoteService) {
   }
 
+  @Input() username: string;
   @Input() deckId: number;
   public form: FormGroup;
   public isSending = false;
 
   public createNote() {
     this.isSending = true;
-    this.noteService.create(this.deckId, this.form.get('content').value, this.getCookie('username'))
+    this.noteService.create(this.deckId, this.form.get('content').value, this.username)
       .subscribe(s => { this.form.reset(); this.isSending = false; });
   }
 
-  private getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var cookies = decodedCookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i].trim();
-      if (cookie.indexOf(name) == 0) {
-        return cookie.substring(name.length, cookie.length);
-      }
-    }
-    return "";
+  ngOnInit() {
+    this.form = this.fb.group({
+      content: this.fb.control({ value: '', disabled: !this.username }, { validators: [Validators.required, Validators.minLength(15), Validators.maxLength(2000)], updateOn: 'blur' })
+    });
   }
 }
