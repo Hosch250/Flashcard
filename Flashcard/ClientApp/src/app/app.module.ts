6982a9996cdf3b0e204b6ebed3e68a05b9a6a631
Abraham Hosch <abraham.hosch@ilmservice.com>
2019-12-30 10:37:33 -0600

    User banner

--- Flashcard/ClientApp/src/app/app.module.ts
+++ Flashcard/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,16 +11,19 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginService } from './shared/services/login.service';
 import { HttpClientModule } from '@angular/common/http';
+import { UserBannerComponent } from './user-banner/user-banner.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        UserBannerComponent
     ],
     imports: [
         BrowserModule,
         MatButtonModule,
         MatIconModule,
         MatToolbarModule,
+        MatMenuModule,
         AppRoutingModule,
         NoopAnimationsModule,
         FlexLayoutModule,
---
+++ Flashcard/ClientApp/src/app/user-banner/user-banner.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { LoginService } from '../shared/services/login.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'fcd-user-banner',
+  templateUrl: './user-banner.component.html',
+  styleUrls: ['./user-banner.component.scss']
+})
+export class UserBannerComponent {
+  constructor(private readonly loginService: LoginService,
+    private readonly router: Router) { }
+
+  public cookieExists(name) {
+    return this.getCookie(name);
+  }
+
+  public username() {
+    return this.getCookie("username");
+  }
+
+  private getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookies = decodedCookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.indexOf(name) == 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
+    }
+    return "";
+  }
+
+  public logout() {
+    this.loginService.logout().subscribe(a => {
+      this.router.navigate(['login']);
+    }, a => {
+      this.router.navigate(['/']);
+    })
+  }
+
+}
